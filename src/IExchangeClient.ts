e98ce9456f1b4812eb97a0497ad29400f3c38ae2
Enrique Plaza <enriplaso@gmail.com>
2025-01-18 10:47:31 +0100

    Updated interface to support promises as return

--- src/IExchangeClient.ts
+++ src/IExchangeClient.ts
@@ -12,7 +12,7 @@ export interface IExchangeClient {
      * @param timeInForce - (Optional) Specifies how long the order remains active. Defaults to "Good Till Canceled".
      * @returns The created order object.
      */
-    marketBuyOrder(funds: number, timeInForce?: TimeInForce, cancelAfter?: Date): Order;
+    marketBuyOrder(funds: number, timeInForce?: TimeInForce, cancelAfter?: Date): Order | Promise<Order>;
 
     /**
      * Places a market sell order to sell cryptocurrency at the current market price.
@@ -21,7 +21,7 @@ export interface IExchangeClient {
      * @param timeInForce - (Optional) Specifies how long the order remains active. Defaults to "Good Till Canceled".
      * @returns The created order object.
      */
-    marketSellOrder(size: number, timeInForce?: TimeInForce, cancelAfter?: Date): Order;
+    marketSellOrder(size: number, timeInForce?: TimeInForce, cancelAfter?: Date): Order | Promise<Order>;
 
     /**
      * Places a limit buy order to purchase cryptocurrency when a specified price is reached or better.
@@ -31,7 +31,7 @@ export interface IExchangeClient {
      * @param timeInForce - (Optional) Specifies how long the order remains active. Defaults to "Good Till Canceled".
      * @returns The created order object.
      */
-    limitBuyOrder(price: number, funds: number, timeInForce?: TimeInForce, cancelAfter?: Date): Order;
+    limitBuyOrder(price: number, funds: number, timeInForce?: TimeInForce, cancelAfter?: Date): Order | Promise<Order>;
 
     /**
      * Places a limit sell order to sell cryptocurrency when a specified price is reached or better.
@@ -41,7 +41,7 @@ export interface IExchangeClient {
      * @param timeInForce - (Optional) Specifies how long the order remains active. Defaults to "Good Till Canceled".
      * @returns The created order object.
      */
-    limitSellOrder(price: number, quantity: number, timeInForce?: TimeInForce, cancelAfter?: Date): Order;
+    limitSellOrder(price: number, quantity: number, timeInForce?: TimeInForce, cancelAfter?: Date): Order | Promise<Order>;
 
     /**
      * Places a stop-loss order to sell cryptocurrency when the price drops to or below a specified level.
@@ -51,7 +51,7 @@ export interface IExchangeClient {
      * @param timeInForce - (Optional) Specifies how long the order remains active. Defaults to "Good Till Canceled".
      * @returns The created order object.
      */
-    stopLossOrder(price: number, size: number, timeInForce?: TimeInForce, cancelAfter?: Date): Order;
+    stopLossOrder(price: number, size: number, timeInForce?: TimeInForce, cancelAfter?: Date): Order | Promise<Order>;
 
     /**
      * Places a stop-entry order to buy cryptocurrency when the price rises to or above a specified level.
@@ -61,38 +61,39 @@ export interface IExchangeClient {
      * @param timeInForce - (Optional) Specifies how long the order remains active. Defaults to "Good Till Canceled".
      * @returns The created order object.
      */
-    stopEntryOrder(price: number, size: number, timeInForce?: TimeInForce, cancelAfter?: Date): Order;
+    stopEntryOrder(price: number, size: number, timeInForce?: TimeInForce, cancelAfter?: Date): Order | Promise<Order>;
 
     /**
      * Cancels a specific order by its unique identifier.
      *
      * @param id - The unique identifier of the order to cancel.
      */
-    cancelOrder(id: string): void;
+    cancelOrder(id: string): void  | Promise<void>;
 
     /**
      * Retrieves all orders currently managed by the client, optionally filtered by their status.
      *
      * @returns An array of orders, optionally filtered by status.
      */
-    getAllOrders(): Order[];
+    getAllOrders(): Order[]  | Promise<Order[]>;
 
     /**
      * Retrieves all trades that have been executed by the client.
      *
      * @returns An array of executed trades.
      */
-    getAllTrades(): Trade[];
+    getAllTrades(): Trade[] | Promise<Trade[]>;
 
     /**
      * Cancels all active orders currently managed by the client.
      */
-    cancelAllOrders(): void;
+    cancelAllOrders(): void | Promise<void>;
 
     /**
      * Retrieves the account information associated with the client, including balance and product quantities.
      *
      * @returns The account details object.
      */
-    getAccount(): Account;
+    getAccount(): Account | Promise<Account>;
+
 }
